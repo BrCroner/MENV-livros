061b34b2af8c1f30363bb6354e2b75ca940b9df2
Bruno Croner <br.croner@gmail.com>
2022-09-25 20:07:34 -0300

    Alterando variáveis de ambiente.

--- backend/src/config/dbConnect.js
+++ backend/src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-mongoose.connect(`mongodb+srv://croner:${porcess.env.MONGODB_PASSWORD}@cluster-node.3ovwqr2.mongodb.net/test`)
+mongoose.connect(`mongodb+srv://croner:${process.env.MONGODB_PASSWORD}@cluster-node.3ovwqr2.mongodb.net/test`)
 
 let db = mongoose.connection
 
